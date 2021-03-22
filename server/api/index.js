2387a34e96843ca636d0825964a7f9f005c256d6
Kush Patel <kushpatel21@gmail.com>
2021-03-22 14:30:42 -0500

    test: test 3

--- server/api/index.js
+++ server/api/index.js
@@ -5,6 +5,7 @@ router.use('/users', require('./users'))
 //router.use('/sessions', require('./sessions'))
 //test 1
 //test 2
+//test 3
 router.use((req, res, next) => {
   const error = new Error('Not Found')
   error.status = 404
