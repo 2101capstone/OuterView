bb0273951b8daaf290eba283ac60135803d62eaf
Kush Patel <kushpatel21@gmail.com>
2021-03-22 14:20:22 -0500

    fix: comment out sessions api route

--- server/api/sessions.js
+++ server/api/sessions.js
@@ -1,23 +1,23 @@
-const router = require('express').Router()
-const {Session} = require('../db/models/Session')
+// const router = require('express').Router()
+// const {Session} = require('../db/models/Session')
 
-router.get('/', async (req, res, next) => {
-  try {
-    const allSessions = await Session.findAll()
-    res.json(allSessions)
-  } catch (error) {
-    next(error)
-  }
-})
+// router.get('/', async (req, res, next) => {
+//   try {
+//     const allSessions = await Session.findAll()
+//     res.json(allSessions)
+//   } catch (error) {
+//     next(error)
+//   }
+// })
 
-router.get('/:id', async (req, res, next) => {
-  try {
-    const {id} = req.params
-    const singleSession = await Session.findByPk(id)
-    res.json(singleSession)
-  } catch (error) {
-    next(error)
-  }
-})
+// router.get('/:id', async (req, res, next) => {
+//   try {
+//     const {id} = req.params
+//     const singleSession = await Session.findByPk(id)
+//     res.json(singleSession)
+//   } catch (error) {
+//     next(error)
+//   }
+// })
 
-module.exports = router
+// module.exports = router
