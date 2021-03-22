48f8a6bc014e7bc7f70bfb3dc4857db3d3b34575
Kush Patel <kushpatel21@gmail.com>
2021-03-22 14:25:49 -0500

    test: test 2 master protection

--- server/api/index.js
+++ server/api/index.js
@@ -4,6 +4,7 @@ module.exports = router
 router.use('/users', require('./users'))
 //router.use('/sessions', require('./sessions'))
 //test 1
+//test 2
 router.use((req, res, next) => {
   const error = new Error('Not Found')
   error.status = 404
