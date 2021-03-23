c4bb423f0d5cca27b67381cb0847ac389559bd20
Kush Patel <kushpatel21@gmail.com>
2021-03-23 10:41:49 -0500

    refactor: add all components to index for easier import and change navbar links

--- client/routes.js
+++ client/routes.js
@@ -2,9 +2,15 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome, PoC, FirebaseTest} from './components'
+import {
+  Login,
+  Signup,
+  UserHome,
+  PoC,
+  FirebaseTest,
+  SpeechToText
+} from './components'
 import {me} from './store'
-import SpeechToText from './components/SpeechToText'
 
 /**
  * COMPONENT
