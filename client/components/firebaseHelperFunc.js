4a3eb3c4811ed8475d537f945f00e92454652c40
Kush Patel <kushpatel21@gmail.com>
2021-03-26 14:44:42 -0500

    feat:ready Firestore for demo

--- client/components/firebaseHelperFunc.js
+++ client/components/firebaseHelperFunc.js
@@ -2,11 +2,13 @@ import firebase from './firebase'
 
 export const handleUpload = async (transcript, fillerWords) => {
   //upload to Firebase
-
+  console.log('check the format:', typeof reactions)
   const res = await firebase
     .firestore()
     .collection('Sessions')
     .add({transcript, fillerWords})
+
+  console.log('Firestore ID:', res.id)
   return res.id
   // .then(() => {
   //   setTitle('')
