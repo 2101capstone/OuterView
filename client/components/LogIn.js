206d08868938718ae8d1a693a0f2bcfb3a041179
Clucas0311 <clucas0311@gmail.com>
2021-03-25 23:47:02 -0400

    Style: Added Google Button

--- client/components/LogIn.js
+++ client/components/LogIn.js
@@ -2,6 +2,7 @@ import React, {useRef, useState} from 'react'
 import {Form, Button, Card, Alert} from 'react-bootstrap'
 import {useAuth} from '../contexts/AuthContext'
 import {Link, useHistory} from 'react-router-dom'
+import {signInWithGoogle} from './firebase/'
 
 const LogIn = () => {
   const emailRef = useRef()
@@ -43,6 +44,9 @@ const LogIn = () => {
             <Button disabled={loading} className="w-100" type="submit">
               Log In
             </Button>
+            <button type="submit" onClick={signInWithGoogle}>
+              Sign In With Google
+            </button>
           </Form>
           <div className="w-100 text-center mt-3">
             <Link to="/forgot-password">Forgot Password?</Link>
