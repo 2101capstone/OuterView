b13bdb4d60ef5e77ab5a302c3f2484de9e6f54af
Clucas0311 <clucas0311@gmail.com>
2021-03-25 23:35:21 -0400

    Feat: Added message hook

--- client/components/ForgotPassword.js
+++ client/components/ForgotPassword.js
@@ -2,10 +2,12 @@ import React, {useRef, useState} from 'react'
 import {Form, Button, Card, Alert} from 'react-bootstrap'
 import {useAuth} from '../contexts/AuthContext'
 import {Link} from 'react-router-dom'
+import {use} from 'chai'
 
 const ForgotPassword = () => {
   const emailRef = useRef()
   const {resetPassword} = useAuth()
+  const [error, setError] = useState('')
   const [message, setMessage] = useState('')
   const [loading, setLoading] = useState(false)
 
@@ -19,7 +21,7 @@ const ForgotPassword = () => {
       await resetPassword(email.Ref.current.value)
       setMessage('Check your inbox for further instructions')
     } catch {
-      setError('Failed to sign in')
+      setError('Failed to reset password')
     }
     setLoading(false)
   }
@@ -30,6 +32,7 @@ const ForgotPassword = () => {
         <Card.Body>
           <h2 className="text-center mb-4">Password Reset</h2>
           {error && <Alert variant="danger">{error}</Alert>}
+          {message && <Alert variant="success">{message}</Alert>}
           <Form onSubmit={handleSubmit}>
             <Form.Group id="email">
               <Form.Label>Email</Form.Label>
