7db957076f013ea23491569e25d00e18f2464a6b
Kush Patel <kushpatel21@gmail.com>
2021-03-30 16:17:19 -0500

    feat: mquery for uid in session document and return match

--- client/components/SavedRecordings.js
+++ client/components/SavedRecordings.js
@@ -1,45 +1,35 @@
 import React, {useEffect, useState} from 'react'
 import {useAuth} from '../contexts/AuthContext'
 import SingleRecordingCard from './SingleRecordingCard'
-import firebase, {storage} from './firebase'
+import firebase from './firebase'
 
 const SavedRecordings = () => {
   const {currentUser} = useAuth() //current user signed in
-  const [sessionIds, setSessionIds] = useState([])
   const [sesDetail, setSesDetail] = useState([])
 
   useEffect(() => {
-    console.log('uid:', currentUser.uid)
-    //Set sessionId from User Doc to state
-    let docRef = firebase
+    let query = firebase
       .firestore()
-      .collection('Users')
-      .doc(currentUser.uid)
-    docRef.get().then(doc => {
-      setSessionIds(doc.data().sessionId)
+      .collection('Sessions')
+      .where('uid', '==', currentUser.uid)
+
+    query.get().then(querySnapshot => {
+      querySnapshot.forEach(doc => {
+        console.log(doc.id, ' => ', doc.data())
+        setSesDetail(prev => [...prev, doc.data()])
+      })
     })
   }, [])
 
-  //Pull
   useEffect(() => {
-    if (sessionIds) {
-      console.log('session Ids:', sessionIds)
-      let docRef = firebase
-        .firestore()
-        .collection('Sessions')
-        .doc(sessionIds[0])
-
-      docRef.get().then(doc => {
-        setSesDetail(doc.data())
-      })
-    }
-  }, [sessionIds])
+    console.log(sesDetail)
+  })
 
   return (
     <div>
       <h1 className="recordings-title">Past Recordings</h1>
       <div className="card mb-3">
-        <SingleRecordingCard fw={sesDetail} />
+        <SingleRecordingCard fw="hello" />
       </div>
     </div>
   )
