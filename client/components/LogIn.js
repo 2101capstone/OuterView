a1ee9d9d03619fc75de8a54debf82e0a4e9fb98a
Clucas0311 <clucas0311@gmail.com>
2021-03-31 11:17:53 -0400

    MoStyle: Style Login Page

--- client/components/LogIn.js
+++ client/components/LogIn.js
@@ -1,5 +1,5 @@
 import React, {useRef, useState} from 'react'
-import {Form, Button, Card, Alert} from 'react-bootstrap'
+import {Form, Button, Card, Alert, Container} from 'react-bootstrap'
 import {useAuth} from '../contexts/AuthContext'
 import {auth, provider} from '../components/firebase'
 import {Link, useHistory} from 'react-router-dom'
@@ -49,44 +49,49 @@ const LogIn = () => {
   }
 
   return (
-    <div>
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">Log In</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
-          <Form onSubmit={handleSubmit}>
-            <Form.Group id="email">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" ref={emailRef} required />
-            </Form.Group>
-            <Form.Group id="password">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" ref={passwordRef} required />
-            </Form.Group>
-            <Button disabled={loading} className="w-100" type="submit">
-              Log In
-            </Button>
-            <Form.Group>
-              <div>
-                <GoogleButton
-                  disabled={loading}
-                  className="w-100"
-                  onClick={signInWithGoogle}
-                >
-                  Sign In With Google
-                </GoogleButton>
-              </div>
-            </Form.Group>
-          </Form>
-          <div className="w-100 text-center mt-3">
-            <Link to="/forgot-password">Forgot Password?</Link>
-          </div>
-        </Card.Body>
-      </Card>
-      <div className="w-100 text-center mt-2">
-        Need an account? <Link to="/signup">Sign Up</Link>
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{minHeight: '100vh'}}
+    >
+      <div className="w-100" style={{maxWidth: '400px'}}>
+        <Card>
+          <Card.Body>
+            <h2 className="text-center mb-4">Log In</h2>
+            {error && <Alert variant="danger">{error}</Alert>}
+            <Form onSubmit={handleSubmit}>
+              <Form.Group id="email">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" ref={emailRef} required />
+              </Form.Group>
+              <Form.Group id="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" ref={passwordRef} required />
+              </Form.Group>
+              <Button disabled={loading} className="w-100" type="submit">
+                Log In
+              </Button>
+              <Form.Group>
+                <div>
+                  <GoogleButton
+                    disabled={loading}
+                    className="w-100"
+                    onClick={signInWithGoogle}
+                  >
+                    Sign In With Google
+                  </GoogleButton>
+                </div>
+              </Form.Group>
+            </Form>
+            <div className="w-100 text-center mt-3">
+              <Link to="/forgot-password">Forgot Password?</Link>
+            </div>
+          </Card.Body>
+        </Card>
+        <div className="w-100 text-center mt-2">
+          Need an account? <Link to="/signup">Sign Up</Link>
+        </div>
       </div>
-    </div>
+    </Container>
   )
 }
 export default LogIn
