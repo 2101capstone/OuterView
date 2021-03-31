3c28633fda02a4966f442502056fc13b129c58a8
Clucas0311 <clucas0311@gmail.com>
2021-03-31 11:22:10 -0400

    Style: Modified the minHeight

--- client/components/SignUp.js
+++ client/components/SignUp.js
@@ -37,7 +37,10 @@ const SignUp = () => {
   }
 
   return (
-    <Container className="d-flex align-items-center justify-content-center">
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{minHeight: '100vh'}}
+    >
       <div className="w-100" style={{maxWidth: '400px'}}>
         <Card>
           <Card.Body>
