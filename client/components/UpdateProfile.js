97033ac0c386329ffd9c6f83a97b1dffe1b9af0b
Clucas0311 <clucas0311@gmail.com>
2021-03-31 16:00:44 -0400

    Feat: Modified UpdateProfile Form

--- client/components/UpdateProfile.js
+++ client/components/UpdateProfile.js
@@ -1,7 +1,91 @@
-import React from 'react'
+import React, {useRef, useState} from 'react'
+import {Form, Button, Card, Alert, Container} from 'react-bootstrap'
+import {Link, useHistory} from 'react-router-dom'
+import {useAuth} from '../contexts/AuthContext'
+import {createUserDoc} from './firebaseHelperFunc'
 
-function UpdateProfile() {
-  return <div></div>
+const UpdateProfile = () => {
+  const emailRef = useRef()
+  const passwordRef = useRef()
+  const passwordConfirmRef = useRef()
+  const {currentUser} = useAuth()
+  const [error, setError] = useState('')
+  const [loading, setLoading] = useState(false)
+  const history = useHistory()
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      return setError('Passwords do not match!')
+    }
+    try {
+      setError('')
+      setLoading(true)
+      const {user} = await signup(
+        emailRef.current.value,
+        passwordRef.current.value
+      )
+      console.log('user', user.uid)
+      // create a new user doc by getting the user
+      createUserDoc(user.uid, {email: emailRef.current.value})
+      history.push('/dashboard')
+    } catch {
+      setError('Failed to create an account')
+    }
+    setLoading(false)
+  }
+
+  return (
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{minHeight: '100vh'}}
+    >
+      <div className="w-100" style={{maxWidth: '400px'}}>
+        <Card>
+          <Card.Body>
+            <h2 className="text-center mb-4">Update Profile</h2>
+            {error && <Alert variant="danger">{error}</Alert>}
+            <Form onSubmit={handleSubmit}>
+              <Form.Group id="email">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  type="email"
+                  ref={emailRef}
+                  required
+                  defaultValue={currentUser.email}
+                />
+              </Form.Group>
+              <Form.Group id="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  ref={passwordRef}
+                  placeholder="Leave blank to keep the same"
+                  required
+                />
+              </Form.Group>
+              <Form.Group id="password-confirm">
+                <Form.Label>Password Confirmation</Form.Label>
+                <Form.Control
+                  type="password"
+                  ref={passwordConfirmRef}
+                  placeholder="Leave blank to keep the same"
+                  required
+                />
+              </Form.Group>
+              <Button disabled={loading} className="w-100" type="submit">
+                Update
+              </Button>
+            </Form>
+          </Card.Body>
+        </Card>
+        <div className="w-100 text-center mt-2">
+          <Link to="/dashboard">Cancel</Link>
+        </div>
+      </div>
+    </Container>
+  )
 }
 
 export default UpdateProfile
