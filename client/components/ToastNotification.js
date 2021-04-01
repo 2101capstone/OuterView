03be59b16ae4a15078236be4acfb6acca6045229
MBusto <mikebusto4@gmail.com>
2021-04-01 16:00:46 -0400

    feat: added react-toastify to toast msg

--- client/components/ToastNotification.js
+++ client/components/ToastNotification.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import {Toast} from 'react-bootstrap'
+import {ToastContainer, toast} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
 const ToastNotification = () => {
+  toast.error('Cannot view page unless Logged in', {
+    position: 'bottom-right',
+    autoClose: 10000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined
+  })
   return (
-    <div>
-      <Toast className="toast-noti">
-        <Toast.Body>YOU MUST BE LOGGED IN TO SEE THIS PAGE</Toast.Body>
-      </Toast>
+    <div className="toast-div">
+      <ToastContainer style={{width: '25%', height: '100px'}} />
     </div>
   )
 }
