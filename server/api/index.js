7805243c2ac20c237be4a71650aad8a8a2301a76
Kush Patel <kushpatel21@gmail.com>
2021-04-01 15:41:29 -0500

    refactor: bare min server code

--- server/api/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router()
-module.exports = router
-
-router.use((req, res, next) => {
-  const error = new Error('Not Found')
-  error.status = 404
-  next(error)
-})
