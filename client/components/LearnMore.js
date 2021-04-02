6bf3f658e8b36af725a738010ff04462afc95967
Kush Patel <kushpatel21@gmail.com>
2021-04-02 17:00:07 -0500

    refactor: delete more unused code;

--- client/components/LearnMore.js
+++ client/components/LearnMore.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Carousel, Card, CardGroup} from 'react-bootstrap'
+import {Carousel} from 'react-bootstrap'
 import {FaGithub, FaLinkedin} from 'react-icons/fa'
 
 const LearnMore = () => {
--- client/components/Scoring.js
+++ client/components/Scoring.js
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export default function scoring(transcript, fillerWords, facialData) {
   let score = {
     finalScore: 0,
