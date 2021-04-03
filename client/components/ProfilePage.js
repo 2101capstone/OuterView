9dd51e2e20c150514f5ec11bcc6076f634a190ce
Clucas0311 <clucas0311@gmail.com>
2021-04-03 18:24:41 -0400

    Style: Profile Page

--- client/components/ProfilePage.js
+++ client/components/ProfilePage.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import {Card, Button, Alert, Container} from 'react-bootstrap'
 import {useAuth} from '../contexts/AuthContext'
 import {Link, useHistory} from 'react-router-dom'
+import '../../public/auth.css'
 
 const ProfilePage = () => {
   const [error, setError] = useState('')
@@ -56,28 +57,35 @@ const ProfilePage = () => {
             />{' '}
           </div>
           <div className="ml-3 w-100">
-            <h4 className="mb-0 mt-0">{currentUser.displayName}</h4>{' '}
+            <h4 className="mb-0 mt-0">{currentUser.displayName}</h4>
+            {error && <Alert variant="danger">{error}</Alert>}
             <div className="p-2 mt-2 bg-primary d-flex justify-content-between rounded text-white stats">
               <div className="d-flex flex-column">
                 {' '}
-                <span className="articles">Articles</span>{' '}
+                <span className="articles">Videos</span>{' '}
                 <span className="number1">38</span>{' '}
               </div>
               <div className="d-flex flex-column">
-                {' '}
                 <span className="followers">Followers</span>{' '}
                 <span className="number2">980</span>{' '}
               </div>
               <div className="d-flex flex-column">
-                {' '}
                 <span className="rating">Rating</span>{' '}
                 <span className="number3">8.9</span>{' '}
               </div>
             </div>
             <div className="button mt-2 d-flex flex-row align-items-center">
-              <button className="btn btn-sm btn-primary w-100 ml-2">
-                Follow
-              </button>{' '}
+              <Link to="/update-profile" className="btn btn-primary w-100 mt-3">
+                Update Profile
+              </Link>
+            </div>
+            <div className="button mt-2 d-flex flex-row align-items-center">
+              <button
+                className="btn btn-sm btn-primary w-100 ml-2"
+                onClick={handleLogout}
+              >
+                Log Out
+              </button>
             </div>
           </div>
         </div>
